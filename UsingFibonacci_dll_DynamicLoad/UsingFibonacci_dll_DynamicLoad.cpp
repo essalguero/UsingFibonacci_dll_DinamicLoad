6d00b31c0f161ac8f97903767d20af7db7c8dbb2
LIVEU-TAD\eugenio.salguero <eugenio.salguero@live.u-tad.com>
2017-12-14 21:55:20 +0100

    no message

--- UsingFibonacci_dll_DynamicLoad/UsingFibonacci_dll_DynamicLoad.cpp
+++ UsingFibonacci_dll_DynamicLoad/UsingFibonacci_dll_DynamicLoad.cpp
@@ -9,24 +9,25 @@
 
 using namespace std;
 
-typedef double(__stdcall* f_fibonacci_DAC)(int);
+//typedef double(__stdcall* f_fibonacci_DAC)(int);
+typedef double(__stdcall* f_fibonacci_DAC)(void);
 
 int main()
 {
 
 	//LPCWSTR LIBRARY_PATH = (LPCWSTR)"Fibonacci_dll.dll";
 
-	HINSTANCE hGetProcIDDLL = LoadLibrary(TEXT("c:\\Library\\Fibonacci_dll.dll"));
+	HINSTANCE hGetProcIDDLL = LoadLibrary(TEXT("c:\\Library\\dll\\Fibonacci_dll.dll"));
 
 	if (!hGetProcIDDLL) {
 		//cout << "Could not load library: " << LIBRARY_PATH << endl << endl; 
-		cout << "Could not load library: c:\\Library\\Fibonacci_dll.dll" << endl << endl;
+		cout << "Could not load library: c:\\Library\\dll\\Fibonacci_dll.dll" << endl << endl;
 
 		return EXIT_FAILURE;
 	}
 
-	f_fibonacci_DAC funcionFibonacci = (f_fibonacci_DAC)GetProcAddress(hGetProcIDDLL, "fibonacci_DivideAndConquer");
-
+	//f_fibonacci_DAC funcionFibonacci = (f_fibonacci_DAC)GetProcAddress(hGetProcIDDLL, "fibonacci_DivideAndConquer");
+	f_fibonacci_DAC funcionFibonacci = (f_fibonacci_DAC)GetProcAddress(hGetProcIDDLL, "fibonacci_Uno");
 	if (!funcionFibonacci) {
 		cout << "Could not load the funcion fibonacci_DivideAndConquer" << endl << endl;
 
@@ -35,7 +36,9 @@ int main()
 		return EXIT_FAILURE;
 	}
 
-	cout << "Fibonacci(46)" << funcionFibonacci(46) << endl << endl;
+	//cout << "Fibonacci(46)" << funcionFibonacci(46) << endl << endl;
+	cout << "Fibonacci()" << funcionFibonacci() << endl << endl;
+	FreeLibrary(hGetProcIDDLL);
 
     return 0;
 }
